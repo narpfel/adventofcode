44b88891b6ddfe41ecf3119a5020e1fdd22437fd
Paul Gey <narpfel@gmx.de>
2018-12-02 18:12:16 +0100

    Solve 2018/02

---
+++ 2018/02/solution.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+from itertools import combinations
+
+
+def part1(lines):
+    letter_counts = [Counter(line) for line in lines]
+    return (
+        sum(2 in c.values() for c in letter_counts)
+        * sum(3 in c.values() for c in letter_counts)
+    )
+
+
+def hamming_distance(word1, word2):
+    return sum(c1 != c2 for c1, c2 in zip(word1, word2))
+
+
+def part2(lines):
+    for word1, word2 in combinations(lines, 2):
+        if hamming_distance(word1, word2) == 1:
+            difference = next(iter(set(word1).difference(word2)))
+            return word1.replace(difference, "")
+
+
+def main():
+    with open("input") as lines:
+        lines = [line.strip() for line in lines]
+
+    print(part1(lines))
+    print(part2(lines))
+
+
+if __name__ == "__main__":
+    main()
