10c30c11e2a6417edf18aa2037ce53881cba328e
Paul Gey <narpfel@gmx.de>
2018-12-02 01:16:53 +0100

    Improve 2018/01 solution by using better combinators

--- 2018/01/src/Main.hs
+++ 2018/01/src/Main.hs
@@ -2,23 +2,19 @@ module Main where
 
 import Data.Foldable (asum)
 import qualified Data.IntSet as IntSet
-import Data.List (unfoldr)
+import Data.List (scanl', mapAccumL)
 import Data.Maybe (fromJust)
 
 partialSums :: Num a => [a] -> [a]
-partialSums xs = unfoldr (uncurry f) (xs, 0)
-  where
-    f (x:xs) s = Just (x + s, (xs, x + s))
-    f [] _ = Nothing
+partialSums = scanl' (+) 0
 
 part2 :: [Int] -> Maybe Int
-part2 input = asum $ unfoldr (uncurry f) (input, mempty)
+part2 = asum . snd . mapAccumL f mempty . partialSums . cycle
   where
-    f (x:xs) set = Just (if x `IntSet.member` set then Just x else Nothing, (xs, IntSet.insert x set))
-    f [] _ = Nothing
+    f set x = (IntSet.insert x set, if x `IntSet.member` set then Just x else Nothing)
 
 main :: IO ()
 main = do
   input <- map read . lines . filter (/= '+') <$> readFile "input" :: IO [Int]
   print . sum $ input
-  print . fromJust . part2 . partialSums . cycle $ input
+  print . fromJust . part2 $ input
