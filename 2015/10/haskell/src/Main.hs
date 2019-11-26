751772a5b4f88296a9a2f48bf55fc0a800fdacd0
Paul Gey <narpfel@gmx.de>
2019-11-26 20:19:13 +0100

    2015/10: Add solution in Haskell

---
+++ 2015/10/haskell/src/Main.hs
@@ -0,0 +1,17 @@
+module Main where
+
+import Data.List (group, iterate')
+
+input :: String
+input = "1113222113"
+
+lookAndSay :: String -> String
+lookAndSay = foldMap (\g -> (show . length $ g) <> [head g]) . group
+
+lookAndSayRecursively :: Int -> String -> Int
+lookAndSayRecursively n = length . (!! n) . iterate' lookAndSay
+
+main :: IO ()
+main = do
+  print . lookAndSayRecursively 40 $ input
+  print . lookAndSayRecursively 50 $ input
