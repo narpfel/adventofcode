3b573fb7a79737b6c50769eb90d147f1a38807a6
Paul Gey <narpfel@gmx.de>
2019-11-26 18:49:08 +0100

    2018/11: Optimise Python solution

--- 2018/11/python/solution.py
+++ 2018/11/python/solution.py
@@ -1,10 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
 
 from collections import namedtuple
 from itertools import chain
 from operator import attrgetter
-
-import numpy as np
 
 
 GRID_SIZE = 300
@@ -15,29 +13,44 @@ INPUT = 7672
 Window = namedtuple("Window", "x, y, window_size, total_power")
 
 
-def windowed(array, window_size):
-    y, x = array.shape
-    for i in range(1, x - window_size + 1):
-        for j in range(1, y - window_size + 1):
-            yield Window(i, j, window_size, array[i:i + window_size, j:j + window_size].sum())
+def windowed(grid, window_size):
+    y = len(grid)
+    x = len(grid[0])
+    for i in range(1, x - window_size):
+        for j in range(1, y - window_size):
+            a = grid[j][i]
+            b = grid[j][i + window_size]
+            c = grid[j + window_size][i + window_size]
+            d = grid[j + window_size][i]
+            power_level = c + a - d - b
+            yield Window(i + 1, j + 1, window_size, power_level)
+
+
+def power_level(x, y, serial_number):
+    rack_id = x + 10
+    return ((((rack_id * y + serial_number) * rack_id) // 100) % 10) - 5
 
 
 def calculate_power_level(serial_number):
-    x = np.arange(GRID_SIZE + 1)
-    y = np.arange(GRID_SIZE + 1)
-    rack_id = x + 10
-    power_level = np.outer(rack_id, y) + serial_number
-    power_level = (power_level.T * rack_id).T
-    power_level //= 100
-    power_level %= 10
-    power_level -= 5
-    return power_level
+    return [
+        [power_level(x, y, serial_number) for x in range(GRID_SIZE + 1)]
+        for y in range(GRID_SIZE + 1)
+    ]
 
 
-def solve(serial_number, window_sizes):
+def precompute_sums(grid):
+    for line in grid:
+        for x in range(1, len(line)):
+            line[x] += line[x - 1]
+    for y in range(1, len(grid)):
+        for x in range(len(grid[y])):
+            grid[y][x] += grid[y - 1][x]
+
+
+def solve(power_levels, window_sizes):
     return max(
         chain.from_iterable(
-            windowed(calculate_power_level(serial_number), window_size)
+            windowed(power_levels, window_size)
             for window_size in window_sizes
         ),
         key=attrgetter("total_power")
@@ -45,9 +58,11 @@ def solve(serial_number, window_sizes):
 
 
 def main():
-    x, y, _, _ = solve(INPUT, [3])
+    power_levels = calculate_power_level(INPUT)
+    precompute_sums(power_levels)
+    x, y, _, _ = solve(power_levels, [3])
     print(f"{x},{y}")
-    x, y, window_size, _ = solve(INPUT, range(1, GRID_SIZE + 1))
+    x, y, window_size, _ = solve(power_levels, range(1, GRID_SIZE + 1))
     print(f"{x},{y},{window_size}")
 
 
