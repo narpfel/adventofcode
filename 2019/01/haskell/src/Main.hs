6e68d7f2d41a0d2ca8313bc9d5ff839e1bca2aba
Paul Gey <narpfel@gmx.de>
2019-12-04 15:43:20 +0100

    Solve 2019/01

---
+++ 2019/01/haskell/src/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+fuelRequirement :: Int -> Int
+fuelRequirement x
+  | x < 0 = 0
+  | otherwise = (x `div` 3) - 2
+
+part1 :: [Int] -> Int
+part1 = sum . map fuelRequirement
+
+part2 :: [Int] -> Int
+part2 = sum . concatMap (takeWhile (> 0) . drop 1 . iterate fuelRequirement)
+
+main :: IO ()
+main = do
+  input <- map read . lines <$> readFile "input"
+  print . part1 $ input
+  print . part2 $ input
