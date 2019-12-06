6ee3e621df3f6c09e0168ea0cff6902ee17ce169
Paul Gey <narpfel@gmx.de>
2019-12-06 23:12:38 +0100

    Solve 2019/03 part 2

--- 2019/03/Solution.kt
+++ 2019/03/Solution.kt
@@ -10,7 +10,7 @@ val directionToMove = mapOf(
 
 fun Pair<Int, Int>.manhattanDistance(): Int = first.absoluteValue + second.absoluteValue
 
-fun track(s: String): Set<Pair<Int, Int>> {
+fun track(s: String): Map<Pair<Int, Int>, Pair<Int, Int>> {
     var position = Pair(0, 0)
     return s.split(',').flatMap {
         val updatePosition = directionToMove.getValue(it[0])
@@ -19,10 +19,14 @@ fun track(s: String): Set<Pair<Int, Int>> {
             position = updatePosition(position)
             position
         }
-    }.toSet()
+    }
+        .mapIndexed { i, p -> p to Pair(p.manhattanDistance(), i + 1) }
+        .toMap()
 }
 
 fun main() {
     val tracks = File("input").readLines().map { track(it) }
-    println(tracks[0].intersect(tracks[1]).map { it.manhattanDistance() }.min())
+    val crossings = tracks[0].keys.intersect(tracks[1].keys)
+    println(crossings.map { tracks[0][it]!!.first }.min())
+    println(crossings.map { tracks[0][it]!!.second + tracks[1][it]!!.second }.min())
 }
