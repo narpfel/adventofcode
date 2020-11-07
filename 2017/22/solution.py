a51b143ddde9fa77580e84769275a6eac7deea9c
Paul Gey <narpfel@gmx.de>
2020-11-07 11:30:46 +0100

    Solve 2017/22 part 2

--- 2017/22/solution.py
+++ 2017/22/solution.py
@@ -1,7 +1,32 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
 from collections import defaultdict
+from enum import IntEnum
 
-STEP_COUNT = 10_000
+STEP_COUNT_PART1 = 10_000
+STEP_COUNT_PART2 = 10_000_000
+
+
+Node = IntEnum("Node", "Clean, Weakened, Infected, Flagged")
+
+
+NEXT_DIRECTION_INDEX = {
+    Node.Infected: 1,
+    Node.Clean: -1,
+    Node.Weakened: 0,
+    Node.Flagged: 2,
+}
+
+NODE_TRANSITION_PART1 = {
+    Node.Infected: Node.Clean,
+    Node.Clean: Node.Infected,
+}
+
+NODE_TRANSITION_PART2 = {
+    Node.Clean: Node.Weakened,
+    Node.Weakened: Node.Infected,
+    Node.Infected: Node.Flagged,
+    Node.Flagged: Node.Clean,
+}
 
 
 def left(position):
@@ -28,27 +53,27 @@ MOVE = [left, up, right, down]
 
 
 def next_direction(cell, direction):
-    return (direction + (1 if cell else -1)) % len(MOVE)
+    return (direction + NEXT_DIRECTION_INDEX[cell]) % len(MOVE)
 
 
-def step(grid, position, direction):
+def step(grid, position, direction, node_transition):
     direction = next_direction(grid[position], direction)
-    grid[position] ^= True
-    has_caused_infection = grid[position]
+    grid[position] = node_transition[grid[position]]
+    has_caused_infection = grid[position] == Node.Infected
     position = MOVE[direction](position)
     return position, direction, has_caused_infection
 
 
 def read_input(input_filename):
-    grid = defaultdict(bool)
+    grid = defaultdict(lambda: Node.Clean)
     with open(input_filename) as lines:
         for y, line in enumerate(lines):
             for x, node in enumerate(line.strip()):
-                grid[(x, y)] = node == "#"
+                grid[(x, y)] = Node.Infected if node == "#" else Node.Clean
     return grid
 
 
-def solve(input_filename):
+def solve(input_filename, step_count, work):
     grid = read_input(input_filename)
     direction = MOVE.index(up)
     size_x = max(x for x, _ in grid)
@@ -58,19 +83,25 @@ def solve(input_filename):
     position = (size_x + 1) // 2, (size_y + 1) // 2
 
     infections_caused = 0
-    for _ in range(STEP_COUNT):
-        position, direction, has_caused_infection = step(grid, position, direction)
+    for _ in range(step_count):
+        position, direction, has_caused_infection = step(grid, position, direction, work)
         infections_caused += has_caused_infection
 
     return infections_caused
 
 
 def test_part1_with_test_input():
-    assert solve("input_test") == 5587
+    assert solve("input_test", STEP_COUNT_PART1, NODE_TRANSITION_PART1) == 5587
+
+
+def test_part2_with_test_input():
+    assert solve("input_test", 100, NODE_TRANSITION_PART2) == 26
+    assert solve("input_test", STEP_COUNT_PART2, NODE_TRANSITION_PART2) == 2511944
 
 
 def main():
-    print(solve("input"))
+    print(solve("input", STEP_COUNT_PART1, NODE_TRANSITION_PART1))
+    print(solve("input", STEP_COUNT_PART2, NODE_TRANSITION_PART2))
 
 
 if __name__ == "__main__":
