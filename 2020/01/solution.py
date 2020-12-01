93f3f93653fddf9e4bebc7d35e2c1ac010b43cd2
Paul Gey <narpfel@gmx.de>
2020-12-01 06:15:55 +0100

    2020/01: refactor

--- 2020/01/solution.py
+++ 2020/01/solution.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 
+from functools import reduce
 from itertools import combinations
+from operator import mul
+
+
+def solve(expenses, length):
+    for values in combinations(expenses, length):
+        if sum(values) == 2020:
+            return reduce(mul, values)
 
 
 def main():
     with open("input") as lines:
         expenses = list(map(int, lines))
 
-    for a, b in combinations(expenses, 2):
-        if a + b == 2020:
-            print(a * b)
-            break
-
-    for a, b, c in combinations(expenses, 3):
-        if a + b + c == 2020:
-            print(a * b * c)
-            break
+    print(solve(expenses, 2))
+    print(solve(expenses, 3))
 
 
 if __name__ == "__main__":
