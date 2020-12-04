45c2c9ae7016f0ec4ca738d0055696e433329ead
Paul Gey <narpfel@gmx.de>
2020-12-04 18:19:00 +0100

    Solve 2020/04 part 2

--- 2020/04/solution.pl
+++ 2020/04/solution.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/env -S swipl -g main -t halt
 
 :- use_module(library(dcg/basics)).
+:- use_module(library(dcg/high_order)).
 
 required_fields([byr, iyr, eyr, hgt, hcl, ecl, pid]).
 
@@ -31,15 +32,41 @@ read_input(Filename, Passports) :-
     string_codes(String, Codes),
     phrase(parse_passports(Passports), Codes).
 
-valid_passport(Passport) :-
-    required_fields(Required),
-    forall(member(Field, Required), memberchk(Field-_, Passport)).
+year(Begin, End) --> integer(Year), { between(Begin, End, Year) }.
 
-part1(Passports, Solution) :-
-    include(valid_passport, Passports, Valid),
+valid_height --> integer(Height), "cm", { between(150, 193, Height) }.
+valid_height --> integer(Height), "in", { between(59, 76, Height) }.
+
+hair_colour --> "#", sequence(xdigit, HexDigits), { length(HexDigits, 6) }.
+
+field_valid(byr-Year) :- phrase(year(1920, 2002), Year).
+field_valid(iyr-Year) :- phrase(year(2010, 2020), Year).
+field_valid(eyr-Year) :- phrase(year(2020, 2030), Year).
+field_valid(hgt-Height) :- phrase(valid_height, Height).
+field_valid(hcl-Colour) :- phrase(hair_colour, Colour).
+field_valid(ecl-ColourString) :-
+    atom_string(Colour, ColourString),
+    memberchk(Colour, [amb, blu, brn, gry, grn, hzl, oth]).
+field_valid(pid-Id) :- phrase(sequence(digit, Digits), Id), length(Digits, 9).
+
+valid_passport(IsFieldValid, Passport) :-
+    required_fields(Required),
+    forall(
+        member(Field, Required),
+        (
+            memberchk(Field-Value, Passport),
+            string_codes(Value, ValueChars),
+            call(IsFieldValid, Field-ValueChars)
+        )
+    ).
+
+solve(Passports, IsFieldValid, Solution) :-
+    include(valid_passport(IsFieldValid), Passports, Valid),
     length(Valid, Solution).
 
 main :-
     read_input(input, Passports),
-    part1(Passports, Part1),
-    write(Part1), nl.
+    solve(Passports, [_] >> true, Part1),
+    write(Part1), nl,
+    solve(Passports, field_valid, Part2),
+    write(Part2), nl.
