a484adff9a37bc5d5ad19f721281a648050ece09
Paul Gey <narpfel@gmx.de>
2020-12-06 12:53:40 +0100

    2020/05: Golf Zsh solution some more

--- 2020/05/zsh/solution.zsh
+++ 2020/05/zsh/solution.zsh
@@ -1,2 +1,2 @@
 #!/usr/bin/env zsh
-n=`sed 'y/FLBR/0011/;s/^/ibase=2;/'<i|sort|bc`;tail -n1<<<$n;comm -23 <(seq `sed -n '1p;$p'<<<$n`) <(<<<$n)
+n(){sed 'y/FLBR/0011/;s/^/ibase=2;/'<i|sort|bc};n|tail -1;n|comm -23 <(seq `n|sed -n '1p;$p'`) -
