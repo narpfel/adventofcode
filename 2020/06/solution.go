0180c207f106623fb3415d94e2a8d639420cd855
Paul Gey <narpfel@gmx.de>
2020-12-06 18:09:06 +0100

    Solve 2020/06 part 2

--- 2020/06/solution.go
+++ 2020/06/solution.go
@@ -7,22 +7,62 @@ import (
 	"strings"
 )
 
-func main() {
+func parseInput(filename string) [][]map[rune]struct{} {
 	input_bytes, err := ioutil.ReadFile("input")
 	if err != nil {
 		log.Fatal(err)
 	}
-	input := string(input_bytes)
+	input := strings.TrimSpace(string(input_bytes))
 
-	groups := strings.Split(input, "\n\n")
-	total := 0
-	for _, group := range groups {
-		set := map[rune]struct{}{}
-		for _, char := range group {
-			set[char] = struct{}{}
+	declarations := [][]map[rune]struct{}{}
+	for _, group := range strings.Split(input, "\n\n") {
+		declaration := []map[rune]struct{}{}
+		for _, line := range strings.Split(group, "\n") {
+			set := map[rune]struct{}{}
+			for _, char := range line {
+				set[char] = struct{}{}
+			}
+			declaration = append(declaration, set)
 		}
-		delete(set, '\n')
-		total += len(set)
+		declarations = append(declarations, declaration)
 	}
-	fmt.Println(total)
+	return declarations
+}
+
+func part1(declarations [][]map[rune]struct{}) int {
+	total := 0
+	for _, group := range declarations {
+		commonAnswers := map[rune]struct{}{}
+		for _, declaration := range group {
+			for key, _ := range declaration {
+				commonAnswers[key] = struct{}{}
+			}
+		}
+		total += len(commonAnswers)
+	}
+	return total
+}
+
+func part2(declarations [][]map[rune]struct{}) int {
+	total := 0
+	for _, group := range declarations {
+		commonAnswers := map[rune]int{}
+		for _, declaration := range group {
+			for key, _ := range declaration {
+				commonAnswers[key]++
+			}
+		}
+		for _, count := range commonAnswers {
+			if count == len(group) {
+				total++
+			}
+		}
+	}
+	return total
+}
+
+func main() {
+	declarations := parseInput("input")
+	fmt.Println(part1(declarations))
+	fmt.Println(part2(declarations))
 }
