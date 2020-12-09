6f0344d2cd67836d8f3f4fa761fdeceaa2b2699d
Paul Gey <narpfel@gmx.de>
2020-12-09 18:06:41 +0100

    Solve 2020/08 part 2

--- 2020/08/Solution.java
+++ 2020/08/Solution.java
@@ -3,6 +3,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.stream.IntStream;
 import java.util.stream.Collectors;
 
 public final class Solution {
@@ -12,9 +14,39 @@ public final class Solution {
         final var instructions = Files.lines(Path.of("input"))
             .map(Instruction::fromString)
             .collect(Collectors.toList());
+        System.out.println(part1(instructions));
+        System.out.println(part2(instructions));
+    }
+
+    private static int part1(final List<Instruction> instructions) {
         final var result = Solution.run(instructions);
         assert(result.type == ResultType.Loop);
-        System.out.println(result.result);
+        return result.result;
+    }
+
+    private static int part2(final List<Instruction> instructions) {
+        return IntStream.range(0, instructions.size())
+            .filter(i -> instructions.get(i).mnemonic != "acc")
+            .mapToObj(i -> {
+                final Instruction instruction = Solution.replace(instructions.get(i));
+                final var newInstructions = new ArrayList<Instruction>(instructions);
+                newInstructions.set(i, instruction);
+                return newInstructions;
+            })
+            .map(newInstructions -> Solution.run(newInstructions))
+            .filter(result -> result.type == ResultType.Ended)
+            .mapToInt(result -> result.result)
+            .findFirst()
+            .getAsInt();
+    }
+
+    private static Instruction replace(final Instruction instruction) {
+        if (instruction.mnemonic == "nop") {
+            return new Instruction("jmp", instruction.parameter);
+        }
+        else {
+            return new Instruction("nop", instruction.parameter);
+        }
     }
 
     private static Result run(final List<Instruction> instructions) {
