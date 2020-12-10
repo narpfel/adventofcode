02e1a6fa95cf8e40c27849bb9d99a52d378ff24d
Paul Gey <narpfel@gmx.de>
2020-12-10 18:49:33 +0100

    Solve 2020/10 part 2

--- 2020/10/solution.js
+++ 2020/10/solution.js
@@ -3,7 +3,7 @@
 const { readFileSync } = require("fs");
 const _ = require("lodash");
 
-const solve = filename => {
+const readInput = filename => {
     const input = readFileSync(filename, "utf-8")
         .trim()
         .split("\n")
@@ -11,14 +11,42 @@ const solve = filename => {
         .sort((a, b) => a - b);
     input.unshift(0);
     input.push(_.max(input) + 3);
+    return input;
+};
 
-    const differences = _.zipWith(input, _.tail(input), (a, b) => b - a);
+const factorial = n => _.range(1, n + 1).reduce(_.multiply, 1);
+
+const choose = n => k => factorial(n) / (factorial(k) * factorial(n - k));
+
+const solvePart1 = input => {
+    const differences = _.zipWith(input, _.tail(input), _.flip(_.subtract));
     const counts = _.countBy(differences);
     return counts[1] * counts[3];
 };
 
+const solvePart2 = input => {
+    const differences = _.zipWith(input, _.tail(input), _.flip(_.subtract));
+
+    const groups = [[differences[0]]];
+    _.forEach(_.tail(differences), diff => {
+        if (_.last(groups)[0] != diff) {
+            groups.push([]);
+        }
+        _.last(groups).push(diff);
+    });
+
+    return _(groups)
+        .filter(group => group[0] == 1)
+        .map('length')
+        .filter(l => l > 1)
+        .map(l => 1 + choose(l)(2))
+        .reduce(_.multiply, 1);
+};
+
 const main = () => {
-    console.log(solve("input"));
+    const input = readInput("input");
+    console.log(solvePart1(input));
+    console.log(solvePart2(input));
 }
 
 main();
