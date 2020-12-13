967ba949b6a1fcf6c709c5dd65b97f6afe83ea43
Paul Gey <narpfel@gmx.de>
2020-12-13 13:24:38 +0100

    2020/02: Improve performance of bash solution by parallelising it.

--- 2020/02/bash/solution.sh
+++ 2020/02/bash/solution.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
-count_character() {
-    character="$1"; shift
-    str="$1"; shift
-}
-
 part1() {
     tr -d ':' < "$1" | while read input; do
-        line=($input)
-        character=${line[1]}
-        password=${line[2]}
-        count=$(tr -cd $character <<< $password | wc -c)
-        range=($(tr - ' ' <<< ${line[0]}))
-        echo $(( $count >= ${range[0]} && $count <= ${range[1]} ))
+        (
+            line=($input)
+            character=${line[1]}
+            password=${line[2]}
+            count=$(tr -cd $character <<< $password | wc -c)
+            range=($(tr - ' ' <<< ${line[0]}))
+            echo $(($count >= ${range[0]} && $count <= ${range[1]}))
+        ) &
     done | grep -c 1
 }
 
 part2() {
     tr -d ':' < "$1" | while read input; do
-        line=($input)
-        character=${line[1]}
-        password=${line[2]}
-        range=($(tr - ' ' <<< ${line[0]}))
-        i=$((${range[0]} - 1))
-        j=$((${range[1]} - 1))
-        x=$(test ${password:$i:1} = $character && echo 1 || echo 0)
-        y=$(test ${password:$j:1} = $character && echo 1 || echo 0)
-        test $x -ne $y && echo 1 || echo 0
+        (
+            line=($input)
+            character=${line[1]}
+            password=${line[2]}
+            range=($(tr - ' ' <<< ${line[0]}))
+            i=$((${range[0]} - 1))
+            j=$((${range[1]} - 1))
+            x=$(test ${password:$i:1} = $character && echo 1 || echo 0)
+            y=$(test ${password:$j:1} = $character && echo 1 || echo 0)
+            test $x -ne $y && echo 1 || echo 0
+        ) &
     done | grep -c 1
 }
 
@@ -36,5 +35,5 @@ main() {
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    main "$@"
+    main
 fi
