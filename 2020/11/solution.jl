578a1c9dacb876c2c588e51cbf8b7cc96a3922b4
Paul Gey <narpfel@gmx.de>
2020-12-13 12:45:20 +0100

    Solve 2020/11 part 2
    Barely, and with abysmal performance. 😸

--- 2020/11/solution.jl
+++ 2020/11/solution.jl
@@ -1,6 +1,8 @@
 #!/usr/bin/env julia
 
-generation(cells, chairs) = begin
+using LinearAlgebra
+
+generation_part1(cells, chairs) = begin
     neighbours = zeros(size(cells))
     for (i, row) in enumerate(eachrow(cells))
         for j in eachindex(row)
@@ -21,22 +23,84 @@ generation(cells, chairs) = begin
 end
 
 
-main() = begin
-    f = open("input")
-    chairs = (
-        readlines(f)
-        |> lines -> reduce(vcat, permutedims.(collect.(lines)))
-        |> chars -> chars .== 'L'
+generation_part2(cells, chairs) = begin
+    neighbours = zeros(size(cells))
+    empty_chairs = chairs .!= cells
+    with_chairs = cells .- empty_chairs
+    for (i, row) in enumerate(eachrow(cells))
+        for j in eachindex(row)
+            # Apparently you cannot `reverse` an empty `BitArray`‽ But an empty
+            # `Array` is fine?
+            lu = diag(reverse(reverse(Array(with_chairs[1:i - 1, 1:j - 1]), dims=1), dims=2))
+            ru = diag(reverse(Array(with_chairs[1:i - 1, j + 1:end]), dims=1))
+            rd = diag(with_chairs[i + 1:end, j + 1:end])
+            ld = diag(reverse(Array(with_chairs[i + 1:end, 1:j - 1]), dims=2))
+            neighbours[i, j] = (
+                # left
+                next_visible_is_alive(reverse(with_chairs[i, 1:j - 1]))
+                # left-up
+                + next_visible_is_alive(lu)
+                # up
+                + next_visible_is_alive(reverse(with_chairs[1:i - 1, j]))
+                # right-up
+                + next_visible_is_alive(ru)
+                # right
+                + next_visible_is_alive(with_chairs[i, j + 1:end])
+                # right-down
+                + next_visible_is_alive(rd)
+                # down
+                + next_visible_is_alive(with_chairs[i + 1:end, j])
+                # left-down
+                + next_visible_is_alive(ld)
+            )
+        end
+    end
+    (
+        (
+            cells
+            .| (.~cells .& (neighbours .== 0))
+        )
+        .& .~(cells .& (neighbours .>= 5))
+        .& chairs
     )
+end
+
+
+slice_square(xss) = xss[1:min(size(xss)...), 1:min(size(xss)...)]
+
+
+next_visible_is_alive(xs) = (
+    xs
+    |> xs -> Iterators.dropwhile(x -> x == 0, xs)
+    |> xs -> if isempty(xs) false else first(xs) == 1 end
+)
+
+
+find_steady_state(chairs, generation) = begin
     cells = falses(size(chairs))
     while true
         new_cells = generation(cells, chairs)
         if new_cells == cells
-            break
+            return cells
         end
         cells = new_cells
     end
-    println(sum(cells))
+end
+
+
+read_input(filename) = open(filename) do io
+    (
+        eachline(io)
+        |> lines -> reduce(vcat, permutedims.(collect.(lines)))
+        |> chars -> chars .== 'L'
+    )
+end
+
+
+main() = begin
+    chairs = read_input("input")
+    println(sum(find_steady_state(chairs, generation_part1)))
+    println(sum(find_steady_state(chairs, generation_part2)))
 end
 
 
