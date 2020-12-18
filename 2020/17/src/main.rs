7640a40d247bff32df3ce642f4fae5adbdaca7d4
Paul Gey <narpfel@gmx.de>
2020-12-18 21:08:18 +0100

    Solve 2020/17 part 1

---
+++ 2020/17/src/main.rs
@@ -0,0 +1,89 @@
+use std::{
+    fs::File,
+    io::{
+        self,
+        BufRead,
+        BufReader,
+    },
+    path::Path,
+};
+
+use itertools::iproduct;
+
+fn next_generation(cells: &[Vec<Vec<bool>>]) -> Vec<Vec<Vec<bool>>> {
+    let x = cells[0][0].len() as isize;
+    let y = cells[0].len() as isize;
+    let z = cells.len() as isize;
+    (-1..=z)
+        .map(|i| {
+            (-1..=y)
+                .map(|j| {
+                    (-1..=x)
+                        .map(|k| {
+                            let cell = (|| {
+                                Some(*cells.get(i as usize)?.get(j as usize)?.get(k as usize)?)
+                            })()
+                            .unwrap_or(false);
+                            let neighbours =
+                                iproduct!((i - 1)..=(i + 1), (j - 1)..=(j + 1), (k - 1)..=(k + 1))
+                                    .filter_map(|(i, j, k)| {
+                                        Some(
+                                            *cells
+                                                .get(i as usize)?
+                                                .get(j as usize)?
+                                                .get(k as usize)?
+                                                as u8,
+                                        )
+                                    })
+                                    .sum::<u8>()
+                                    - cell as u8;
+                            if cell {
+                                neighbours == 2 || neighbours == 3
+                            }
+                            else {
+                                neighbours == 3
+                            }
+                        })
+                        .collect()
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn read_input(filename: impl AsRef<Path>) -> io::Result<Vec<Vec<Vec<bool>>>> {
+    let file = File::open(filename)?;
+    Ok(vec![
+        BufReader::new(file)
+            .lines()
+            .map(|line| {
+                let line = line?;
+                Ok(line
+                    .strip_suffix(char::is_whitespace)
+                    .unwrap_or(&line)
+                    .chars()
+                    .map(|c| c == '#')
+                    .collect())
+            })
+            .collect::<io::Result<_>>()?,
+    ])
+}
+
+fn main() -> io::Result<()> {
+    let mut cells = read_input("input")?;
+    for _ in 0..6 {
+        cells = next_generation(&cells);
+    }
+    println!(
+        "{}",
+        cells
+            .into_iter()
+            .map(|plane| plane
+                .into_iter()
+                .map(|row| row.into_iter().map(|cell| cell as u64).sum::<u64>())
+                .sum::<u64>())
+            .sum::<u64>()
+    );
+
+    Ok(())
+}
