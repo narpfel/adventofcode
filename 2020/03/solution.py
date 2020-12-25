c9d573a995275cf5efce93945ad2662e274694ff
Paul Gey <narpfel@gmx.de>
2020-12-25 21:19:03 +0100

    2020/03: `enumerate(...)` is the same as `zip(count(), ...)`

--- 2020/03/solution.py
+++ 2020/03/solution.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from functools import reduce
-from itertools import count
 from itertools import islice
 from operator import mul
 
@@ -9,7 +8,7 @@ from operator import mul
 def count_encountered_trees(tree_map, dx, dy):
     return sum(
         line[(dx * x) % len(line)] == "#"
-        for (x, line) in zip(count(), islice(tree_map, None, None, dy))
+        for (x, line) in enumerate(islice(tree_map, None, None, dy))
     )
 
 
