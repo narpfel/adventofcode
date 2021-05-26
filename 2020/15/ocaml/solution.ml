3834957ce99f14d54e4379b0f7e01d00b991a009
Paul Gey <narpfel@gmx.de>
2021-05-26 15:59:29 +0200

    2020/15: Use `Bigarray` in OCaml solution

--- 2020/15/ocaml/solution.ml
+++ 2020/15/ocaml/solution.ml
@@ -1,12 +1,15 @@
+module Array = Bigarray.Array1
+
 let input = [16; 12; 1; 0; 15; 7; 11]
 
 let solve starting_numbers turn_count =
-    let number_to_turn = Array.make turn_count (-1) in
-    List.iteri (fun i n -> Array.set number_to_turn n i) input;
+    let number_to_turn = Array.create Bigarray.int32 Bigarray.c_layout turn_count in
+    Array.fill number_to_turn (-1l);
+    List.iteri (fun i n -> Array.set number_to_turn n (Int32.of_int i)) input;
     let last_spoken = List.length starting_numbers |> pred |> List.nth starting_numbers |> ref in
     for turn = List.length starting_numbers to pred turn_count do
-        let last_spoken_on_turn = Array.get number_to_turn !last_spoken in
-        Array.set number_to_turn !last_spoken (pred turn);
+        let last_spoken_on_turn = Array.get number_to_turn !last_spoken |> Int32.to_int in
+        Array.set number_to_turn !last_spoken (turn |> pred |> Int32.of_int);
         last_spoken := if last_spoken_on_turn < 0 then 0 else turn - last_spoken_on_turn - 1
     done;
     !last_spoken
