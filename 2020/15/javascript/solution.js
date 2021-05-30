0ae1faeafbdb5d23baa92f1711a3cfb93f8a9aba
Paul Gey <narpfel@gmx.de>
2021-05-30 15:15:39 +0200

    2020/15: Add solution in JavaScript

---
+++ 2020/15/javascript/solution.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+const input = [16, 12, 1, 0, 15, 7, 11];
+
+const solve = (starting_numbers, turn_count) => {
+    const number_to_turn = new Array(turn_count);
+    number_to_turn.fill(-1);
+    for (const [i, n] of starting_numbers.entries()) {
+        number_to_turn[n] = i;
+    }
+    let last_spoken = starting_numbers[starting_numbers.length - 1];
+    for (let turn = starting_numbers.length; turn < turn_count; ++turn) {
+        const last_spoken_on_turn = number_to_turn[last_spoken];
+        number_to_turn[last_spoken] = turn - 1;
+        if (last_spoken_on_turn < 0) {
+            last_spoken = 0;
+        }
+        else {
+            last_spoken = turn - last_spoken_on_turn - 1;
+        }
+    }
+    return last_spoken;
+};
+
+const main = () => {
+    console.log(solve(input, 2020));
+    console.log(solve(input, 30_000_000));
+};
+
+main();
