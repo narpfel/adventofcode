b4261116815fa678a8ccff3f0582053e42b81f0e
Paul Gey <narpfel@gmx.de>
2021-06-15 14:37:11 +0200

    2020/15: Improve performance of Java solution

--- 2020/15/java/Solution.java
+++ 2020/15/java/Solution.java
@@ -1,36 +1,26 @@
 import java.util.ArrayList;
 
 public final class Solution {
-    private static class Pair {
-        public final int x;
-        public final int y;
-
-        public Pair(final int x, final int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
-
     final static int[] INPUT = {16, 12, 1, 0, 15, 7, 11};
 
     private static int solve(final int[] startingNumbers, final int turnCount) {
-        final var numberToTurns = new ArrayList<Pair>(turnCount);
+        final var numberToTurn = new ArrayList<Integer>(turnCount);
         for (int i = 0; i < turnCount; ++i) {
-            numberToTurns.add(new Pair(-1, -1));
+            numberToTurn.add(-1);
         }
         for (int i = 0; i < startingNumbers.length; ++i) {
-            numberToTurns.set(startingNumbers[i], new Pair(i, -1));
+            numberToTurn.set(startingNumbers[i], i);
         }
         var lastSpoken = startingNumbers[startingNumbers.length - 1];
         for (int turn = startingNumbers.length; turn < turnCount; ++turn) {
-            final var p = numberToTurns.get(lastSpoken);
-            if (p.y == -1) {
+            final var lastSpokenOnTurn = numberToTurn.get(lastSpoken);
+            numberToTurn.set(lastSpoken, turn - 1);
+            if (lastSpokenOnTurn == -1) {
                 lastSpoken = 0;
             }
             else {
-                lastSpoken = p.x - p.y;
+                lastSpoken = turn - lastSpokenOnTurn - 1;
             }
-            numberToTurns.set(lastSpoken, new Pair(turn, numberToTurns.get(lastSpoken).x));
         }
         return lastSpoken;
     }
