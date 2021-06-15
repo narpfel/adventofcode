aa162d826c2c74921b270947efdc673525cb35ec
Paul Gey <narpfel@gmx.de>
2021-06-15 15:09:51 +0200

    md5: Add binary crate for testing purposes

---
+++ 2016/14/md5/src/main.rs
@@ -0,0 +1,18 @@
+use std::{
+    env::args,
+    error::Error,
+    fs::File,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let filename = args().nth(1).expect("Usage: md5 <filename>");
+    let file = File::open(&filename)?;
+    let bytes = unsafe { memmap::Mmap::map(&file) }?;
+    println!(
+        "{}  {}",
+        std::str::from_utf8(&md5::format_digest(md5::md5(&bytes)))?,
+        filename,
+    );
+
+    Ok(())
+}
