3e47fe7ecd4802e94399f4adabbc89914ade38f3
Paul Gey <narpfel@gmx.de>
2021-06-20 22:43:56 +0200

    2016/05: Improve performance by using PyPy and `_md5`

--- 2016/05/solution.py
+++ 2016/05/solution.py
@@ -1,35 +1,35 @@
-#!/usr/bin/env python3
-from hashlib import md5
+#!/usr/bin/env pypy3
 from itertools import count
-from itertools import islice
+
+from _md5 import md5
 
 INPUT = b"reyedfim"
 
 
-def byte_after_leading_zeroes(leading_zeroes):
+def byte_after_leading_zeroes(leading_zeroes_count):
+    leading_zeroes = "0" * leading_zeroes_count
     for n in count():
         hash = md5()
         hash.update(INPUT + str(n).encode())
         digest = hash.hexdigest()
-        if digest.startswith("0" * leading_zeroes):
-            yield digest[leading_zeroes], digest[leading_zeroes + 1]
+        if digest.startswith(leading_zeroes):
+            yield digest[leading_zeroes_count], digest[leading_zeroes_count + 1]
 
 
 def main():
-    print("".join(byte for byte, _ in islice(byte_after_leading_zeroes(5), 8)))
+    part1 = []
+    part2 = {}
 
-    password = {}
     for position, byte in byte_after_leading_zeroes(5):
-        try:
-            position = int(position)
-        except ValueError:
-            pass
-        else:
-            if position in range(8):
-                password.setdefault(position, byte)
-        if len(password) == 8:
-            print("".join(byte for _, byte in sorted(password.items())))
+        if len(part1) < 8:
+            part1.append(position)
+        if '0' <= position < '8':
+            part2.setdefault(position, byte)
+        if len(part2) == 8:
             break
+
+    print("".join(part1))
+    print("".join(byte for _, byte in sorted(part2.items())))
 
 
 if __name__ == "__main__":
