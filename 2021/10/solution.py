c344fe4e4cca2eed3ad3ee003492ec82d3e9b5f8
Paul Gey <narpfel@gmx.de>
2021-12-11 16:20:28 +0100

    Solve 2021/10 part 2

--- 2021/10/solution.py
+++ 2021/10/solution.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+from functools import reduce
+from statistics import median
+
+from more_itertools import partition
+
 PARENS = dict(["()", "[]", "{}", "<>"])
 OPENING = frozenset(PARENS.keys())
 CLOSING = frozenset(PARENS.values())
@@ -9,9 +14,33 @@ ERROR_SCORE = {
     "}": 1197,
     ">": 25137,
 }
+AUTOCOMPLETE_SCORE = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
+}
 
 
-def first_mismatch(line):
+class Incomplete:
+    def __init__(self, autocompletion):
+        self.autocompletion = autocompletion
+
+    @property
+    def score(self):
+        return reduce(lambda acc, c: 5 * acc + AUTOCOMPLETE_SCORE[c], self.autocompletion, 0)
+
+
+class Corrupted:
+    def __init__(self, mismatch):
+        self.mismatch = mismatch
+
+    @property
+    def score(self):
+        return ERROR_SCORE[self.mismatch]
+
+
+def analyse(line):
     open = []
     for c in line:
         if c in OPENING:
@@ -19,18 +48,25 @@ def first_mismatch(line):
         elif c in CLOSING:
             expected = open.pop()
             if PARENS[expected] != c:
-                return c
+                return Corrupted(c)
         else:
             raise AssertionError(f"unexpected character {c!r}")
-    return None
+    return Incomplete([PARENS[c] for c in reversed(open)])
 
 
 def main():
     with open("input") as lines:
         lines = [line.strip() for line in lines]
 
-    syntax_error_score = sum(ERROR_SCORE.get(first_mismatch(line), 0) for line in lines)
+    incomplete, corrupted = partition(
+        lambda value: isinstance(value, Corrupted),
+        map(analyse, lines),
+    )
+
+    syntax_error_score = sum(mismatch.score for mismatch in corrupted)
     print(syntax_error_score)
+    autocomplete_score = median(autocompletion.score for autocompletion in incomplete)
+    print(autocomplete_score)
 
 
 if __name__ == "__main__":
