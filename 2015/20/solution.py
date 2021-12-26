475d40eabe75160dcc018f843172928a693a947c
Paul Gey <narpfel@gmx.de>
2021-12-26 17:58:57 +0100

    2015/20: PyPy now supports underscores in int literals

--- 2015/20/solution.py
+++ 2015/20/solution.py
@@ -8,10 +8,10 @@ def main():
     part_1_solved = False
     for n in count(1):
         divisors = sympy.divisors(n)
-        if not part_1_solved and 10 * sum(divisors) >= 36000000:
+        if not part_1_solved and 10 * sum(divisors) >= 36_000_000:
             print("Solution (part 1):", n)
             part_1_solved = True
-        if 11 * sum(div for div in divisors if n / div <= 50) >= 36000000:
+        if 11 * sum(div for div in divisors if n / div <= 50) >= 36_000_000:
             print("Solution (part 2):", n)
             break
 
