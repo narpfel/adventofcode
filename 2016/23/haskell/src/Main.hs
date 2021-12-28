2d1a7f88f832559473cec8dc2fb6e542d2110f6f
Paul Gey <narpfel@gmx.de>
2021-12-28 15:27:59 +0100

    2016/23: Remove obsolete TODO for Haskell solution

--- 2016/23/haskell/src/Main.hs
+++ 2016/23/haskell/src/Main.hs
@@ -14,7 +14,4 @@ main :: IO ()
 main = do
   Just p <- fmap Vector.fromList . parse program <$> readFile "input"
   print $ solve 7 p
-  -- TODO: This takes ~45 seconds. Implement macro-op fusion for inc/dec/jnz → add
-  -- and cpy/add/dec/jnz → mul. `tgl` requires unfusing, applying the toggle and then re-fusing.
-  -- Pad with `nop`s to keep jump targets stable, jumping into the loop should also unfuse.
   print $ solve 12 p
