28c5401dbc021c9da494a033bca4bc0b77599d82
Paul Gey <narpfel@gmx.de>
2022-12-02 06:15:33 +0100

    Solve 2022/02 part 2

--- 2022/02/solution.py
+++ 2022/02/solution.py
@@ -12,12 +12,25 @@ SCORE_PART_1 = {
     "C Z": 6,
 }
 
+SCORE_PART_2 = {
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7,
+}
+
 
 def main():
     with open("input") as input_file:
         strategy_guide = [line.strip() for line in input_file]
 
     print(sum(SCORE_PART_1[round] for round in strategy_guide))
+    print(sum(SCORE_PART_2[round] for round in strategy_guide))
 
 
 if __name__ == "__main__":
