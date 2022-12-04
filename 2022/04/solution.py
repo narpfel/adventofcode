df183a300155603f5a87d6867eb00cf160b38b37
Paul Gey <narpfel@gmx.de>
2022-12-04 06:16:40 +0100

    Solve 2022/04 part 2

--- 2022/04/solution.py
+++ 2022/04/solution.py
@@ -2,6 +2,8 @@
 
 EXPECTED_PART_1 = 2
 
+EXPECTED_PART_2 = 4
+
 
 def inclusive_range(lo, hi):
     return range(lo, hi + 1)
@@ -21,14 +23,24 @@ def part_1(assignments):
     return sum(lhs >= rhs or rhs >= lhs for lhs, rhs in assignments)
 
 
+def part_2(assignments):
+    return sum(not lhs.isdisjoint(rhs) for lhs, rhs in assignments)
+
+
 def test_part_1():
     assignments = read_input("input_test")
     assert part_1(assignments) == EXPECTED_PART_1
 
 
+def test_part_2():
+    assignments = read_input("input_test")
+    assert part_2(assignments) == EXPECTED_PART_2
+
+
 def main():
     assignments = list(read_input("input"))
     print(part_1(assignments))
+    print(part_2(assignments))
 
 
 if __name__ == "__main__":
