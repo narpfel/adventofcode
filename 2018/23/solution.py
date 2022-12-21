8d8a915276152912d4c8e7c8fc28472707eb88aa
Paul Gey <narpfel@gmx.de>
2022-12-21 21:15:03 +0100

    Solve 2018/23 part 2

--- 2018/23/solution.py
+++ 2018/23/solution.py
@@ -3,6 +3,8 @@
 import re
 from operator import attrgetter
 
+import z3
+
 NANOBOT_RE = re.compile(r"pos=<(-?\d+),(-?\d+),(-?\d+)>, r=(\d+)")
 
 
@@ -31,12 +33,36 @@ def part_1(nanobots):
     return sum(other.distance_to(nanobot) <= nanobot.radius for other in nanobots)
 
 
+def manhattan_distance(p1, p2):
+    return z3.Sum([z3.Abs(a - b) for a, b in zip(p1, p2)])
+
+
+def part_2(nanobots):
+    opt = z3.Optimize()
+    point = z3.Int("p0"), z3.Int("p1"), z3.Int("p2")
+    in_range = [
+        z3.If(manhattan_distance(point, nanobot.position) <= nanobot.radius, 1, 0)
+        for nanobot in nanobots
+    ]
+    opt.maximize(z3.Sum(in_range))
+    opt.minimize(manhattan_distance(point, (0, 0, 0)))
+    assert opt.check() == z3.sat
+    model = opt.model()
+    return sum(abs(model[x].as_long()) for x in point)
+
+
 def test_part_1():
     assert part_1(read_input("input_test")) == 7
 
 
+def test_part_2():
+    assert part_2(read_input("input_test_2")) == 36
+
+
 def main():
-    print(part_1(read_input("input")))
+    nanobots = read_input("input")
+    print(part_1(nanobots))
+    print(part_2(nanobots))
 
 
 if __name__ == "__main__":
