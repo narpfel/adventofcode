5009a39908bdb7f4dbb46e6d047f54d6e71d73db
Paul Gey <narpfel@gmx.de>
2023-12-01 06:38:00 +0100

    Solve 2023/01 part 2
    Damn overlaps!

--- 2023/01/solution.py
+++ 2023/01/solution.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python3
 
+import re
 from string import digits as DIGITS
 
+from more_itertools import flatten
+
 EXPECTED_PART_1 = 142
+EXPECTED_PART_2 = 281
+
+DIGITS_AND_WORD_DIGITS = (
+    "zero_not_included one two three four five six seven eight nine "
+    "0_not_included 1 2 3 4 5 6 7 8 9"
+).split()
+
+DIGITS_RE = re.compile("|".join(f"(?=({digit}))" for digit in DIGITS_AND_WORD_DIGITS))
 
 
 def read_input(filename):
@@ -18,13 +29,30 @@ def part_1(lines):
     return result
 
 
+def part_2(lines):
+    result = 0
+    for line in lines:
+        digits = list(filter(bool, flatten(DIGITS_RE.findall(line))))
+        result += (
+            10 * (DIGITS_AND_WORD_DIGITS.index(digits[0]) % 10)
+            + (DIGITS_AND_WORD_DIGITS.index(digits[-1]) % 10)
+        )
+    return result
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test_2")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     print(part_1(read_input("input")))
+    print(part_2(read_input("input")))
 
 
 if __name__ == "__main__":
