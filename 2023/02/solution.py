d0a56ef9c79bf533bd47e2bca402fe6e42843946
Paul Gey <narpfel@gmx.de>
2023-12-02 06:26:40 +0100

    Solve 2023/02 part 1

---
+++ 2023/02/solution.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from collections import Counter
+
+EXPECTED_PART_1 = 8
+
+
+def read_input(filename):
+    with open(filename) as lines:
+        for line in lines:
+            game, handfuls = line.split(":")
+            game_id = int(game.split()[-1])
+            yield game_id, [
+                Counter({
+                    colour: int(count)
+                    for count, colour in map(str.split, handful.split(", "))
+                })
+                for handful in handfuls.split("; ")
+            ]
+
+
+def part_1(games):
+    return sum(
+        game_id
+        for game_id, counts in games
+        if all(c < Counter({"red": 12, "green": 13, "blue": 14}) for c in counts)
+    )
+
+
+def test_part_1():
+    puzzle_input = read_input("input_test")
+    assert part_1(puzzle_input) == EXPECTED_PART_1
+
+
+def main():
+    print(part_1(read_input("input")))
+
+
+if __name__ == "__main__":
+    main()
