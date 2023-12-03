982f12ae2ffe036d78d433c42a1dc06baa2b02e9
Paul Gey <narpfel@gmx.de>
2023-12-03 06:46:56 +0100

    Solve 2023/03 part 2

--- 2023/03/solution.py
+++ 2023/03/solution.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import math
 import re
+from collections import defaultdict
 from itertools import product
 
 EXPECTED_PART_1 = 4361
+EXPECTED_PART_2 = 467835
 
 SYMBOL_RE = re.compile(r"[^\d\.]")
 
@@ -37,14 +40,35 @@ def part_1(lines, numbers):
     )
 
 
+def part_2(lines, numbers):
+    gears = defaultdict(list)
+
+    for j, match in numbers:
+        for J, I in adjacent_indices(lines, j, match.span()):
+            if lines[J][I] == "*":
+                gears[J, I].append(int(match[0]))
+
+    return sum(
+        math.prod(gear)
+        for gear in gears.values()
+        if len(gear) == 2
+    )
+
+
 def test_part_1():
     lines, numbers = read_input("input_test")
     assert part_1(lines, numbers) == EXPECTED_PART_1
 
 
+def test_part_2():
+    lines, numbers = read_input("input_test")
+    assert part_2(lines, numbers) == EXPECTED_PART_2
+
+
 def main():
     lines, numbers = read_input("input")
     print(part_1(lines, numbers))
+    print(part_2(lines, numbers))
 
 
 if __name__ == "__main__":
