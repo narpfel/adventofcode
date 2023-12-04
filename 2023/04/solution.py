c228954732f8e25d0f1728b30ec80872ac260b29
Paul Gey <narpfel@gmx.de>
2023-12-04 06:30:48 +0100

    Solve 2023/04 part 2

--- 2023/04/solution.py
+++ 2023/04/solution.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 EXPECTED_PART_1 = 13
+EXPECTED_PART_2 = 30
 
 
 def read_input(filename):
@@ -21,13 +22,30 @@ def part_1(cards):
     )
 
 
+def part_2(cards):
+    cards = list(cards)
+    card_amounts = [1] * len(cards)
+    for i, (numbers_i_have, winning_numbers) in enumerate(cards):
+        winning_numbers_i_have = numbers_i_have & winning_numbers
+        for j in range(i + 1, i + len(winning_numbers_i_have) + 1):
+            card_amounts[j] += card_amounts[i]
+
+    return sum(card_amounts)
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     print(part_1(read_input("input")))
+    print(part_2(read_input("input")))
 
 
 if __name__ == "__main__":
