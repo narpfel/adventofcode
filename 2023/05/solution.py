abe66da9072de6c0f808c7863e214c9428c7a641
Paul Gey <narpfel@gmx.de>
2023-12-05 07:19:45 +0100

    Solve 2023/05 part 2

--- 2023/05/solution.py
+++ 2023/05/solution.py
@@ -2,7 +2,11 @@
 
 from collections import namedtuple
 
+import z3
+from more_itertools import chunked
+
 EXPECTED_PART_1 = 35
+EXPECTED_PART_2 = 46
 
 
 Range = namedtuple("Range", "dest, src, len")
@@ -38,14 +42,53 @@ def part_1(seeds, maps):
     return min(locations)
 
 
+def part_2(seeds, maps):
+    opt = z3.Optimize()
+
+    values = {"seed": z3.Int("seed")} | {
+        to: z3.Int(to)
+        for _, to in maps
+    }
+    seed = values["seed"]
+
+    constraints = [
+        z3.And(seed >= seed_range[0], seed < seed_range[0] + seed_range[1])
+        for seed_range in chunked(seeds, 2)
+    ]
+    opt.add(z3.Or(*constraints))
+
+    for (from_, to), map_ in maps.items():
+        from_value = values[from_]
+        to_value = values[to]
+        calculation = from_value == to_value
+        for range_ in map_:
+            calculation = z3.If(
+                z3.And(from_value >= range_.src, from_value < range_.src + range_.len),
+                to_value == from_value - range_.src + range_.dest,
+                calculation,
+            )
+        opt.add(calculation)
+
+    opt.minimize(values["location"])
+    assert opt.check() == z3.sat
+    model = opt.model()
+    return model[values["location"]]
+
+
 def test_part_1():
     seeds, maps = read_input("input_test")
     assert part_1(seeds, maps) == EXPECTED_PART_1
 
 
+def test_part_2():
+    seeds, maps = read_input("input_test")
+    assert part_2(seeds, maps) == EXPECTED_PART_2
+
+
 def main():
     seeds, maps = read_input("input")
     print(part_1(seeds, maps))
+    print(part_2(seeds, maps))
 
 
 if __name__ == "__main__":
