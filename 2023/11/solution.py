e25d17e6a7271f566c72693e2b0481c45a090741
Paul Gey <narpfel@gmx.de>
2023-12-11 06:35:45 +0100

    Solve 2023/11 part 2

--- 2023/11/solution.py
+++ 2023/11/solution.py
@@ -46,14 +46,25 @@ def part_1(universe):
     return distances_stretched(universe, 1)
 
 
+def part_2(universe):
+    return distances_stretched(universe, 1_000_000 - 1)
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert distances_stretched(puzzle_input, 10 - 1) == 1030
+    assert distances_stretched(puzzle_input, 100 - 1) == 8410
+
+
 def main():
     universe = read_input("input")
     print(part_1(universe))
+    print(part_2(universe))
 
 
 if __name__ == "__main__":
