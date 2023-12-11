6c1d3ff6a706ad0b253f856296918f4ea1e6b3e6
Paul Gey <narpfel@gmx.de>
2023-12-11 06:34:39 +0100

    Solve 2023/11 part 1

---
+++ 2023/11/solution.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env pypy3
+
+from collections import namedtuple
+from itertools import combinations
+
+EXPECTED_PART_1 = 374
+
+Universe = namedtuple("Universe", "galaxies, empty_x, empty_y")
+
+
+def read_input(filename):
+    with open(filename) as lines:
+        galaxies = {
+            (x, y)
+            for y, line in enumerate(lines)
+            for x, c in enumerate(line.strip())
+            if c == "#"
+        }
+    max_x = max(x for x, _ in galaxies)
+    max_y = max(y for _, y in galaxies)
+    empty_x = {
+        x
+        for x in range(max_x + 1)
+        if not any((x, y) in galaxies for y in range(max_y + 1))
+    }
+    empty_y = {
+        y
+        for y in range(max_y + 1)
+        if not any((x, y) in galaxies for x in range(max_x + 1))
+    }
+    return Universe(galaxies, empty_x, empty_y)
+
+
+def distances_stretched(universe, stretch):
+    return sum(
+        abs(x - X) + abs(y - Y)
+        + stretch * (
+            sum(a in universe.empty_x for a in range(min(x, X), max(x, X)))
+            + sum(a in universe.empty_y for a in range(min(y, Y), max(y, Y)))
+        )
+        for (x, y), (X, Y) in combinations(universe.galaxies, r=2)
+    )
+
+
+def part_1(universe):
+    return distances_stretched(universe, 1)
+
+
+def test_part_1():
+    puzzle_input = read_input("input_test")
+    assert part_1(puzzle_input) == EXPECTED_PART_1
+
+
+def main():
+    universe = read_input("input")
+    print(part_1(universe))
+
+
+if __name__ == "__main__":
+    main()
