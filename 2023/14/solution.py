d89db62e459fea106b9e698c91367360bacb3265
Paul Gey <narpfel@gmx.de>
2023-12-14 21:19:15 +0100

    Solve 2023/14 part 2

--- 2023/14/solution.py
+++ 2023/14/solution.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env pypy3
 
+from itertools import count
 from itertools import product
 
 EXPECTED_PART_1 = 136
+EXPECTED_PART_2 = 64
+
+TOTAL_CYCLE_COUNT = 1000000000
 
 
 def read_input(filename):
@@ -42,13 +46,60 @@ def part_1(puzzle_input):
     )
 
 
+def part_2(puzzle_input):
+    (max_x, max_y), round, square = puzzle_input
+    xrange = range(max_x + 1)
+    yrange = range(max_y + 1)
+    seen = {}
+
+    for i in count():
+        frozen_round = frozenset(round)
+        if frozen_round in seen:
+            break
+        else:
+            seen[frozen_round] = i
+
+        for x_offset, y_offset, all_points in [
+            (0, -1, product(yrange, xrange)),
+            (-1, 0, product(yrange, xrange)),
+            (0, 1, product(reversed(yrange), xrange)),
+            (1, 0, product(yrange, reversed(xrange))),
+        ]:
+            for y, x in all_points:
+                if (x, y) in round:
+                    round.remove((x, y))
+                    while True:
+                        rock = x, y = x + x_offset, y + y_offset
+                        if rock in round or rock in square or x not in xrange or y not in yrange:
+                            x, y = x - x_offset, y - y_offset
+                            break
+                    round.add((x, y))
+
+    seen_at = {
+        v: k for k, v in seen.items()
+    }
+    cycle_start = seen[frozen_round]
+    cycle_length = i - cycle_start
+    rest = (TOTAL_CYCLE_COUNT - cycle_start) % cycle_length
+    return sum(
+        max_y + 1 - y
+        for _, y in seen_at[cycle_start + rest]
+    )
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     print(part_1(read_input("input")))
+    print(part_2(read_input("input")))
 
 
 if __name__ == "__main__":
