d992845fc61d83da2cb449c17dc223663273d54e
Paul Gey <narpfel@gmx.de>
2023-12-15 06:23:23 +0100

    Solve 2023/15 part 2

--- 2023/15/solution.py
+++ 2023/15/solution.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+import re
+from collections import defaultdict
+from contextlib import suppress
+
 EXPECTED_PART_1 = 1320
+EXPECTED_PART_2 = 145
+
+NAME_RE = re.compile(r"\w+")
 
 
 def read_input(filename):
@@ -21,14 +28,40 @@ def part_1(initialisation_sequence):
     return sum(calculate_hash(step) for step in initialisation_sequence)
 
 
+def part_2(initialisation_sequence):
+    boxes = defaultdict(dict)
+
+    for operation in initialisation_sequence:
+        name = NAME_RE.match(operation)[0]
+        hash_value = calculate_hash(name)
+        if "-" in operation:
+            with suppress(KeyError):
+                del boxes[hash_value][name]
+        if "=" in operation:
+            focus_length = int(operation.split("=")[1])
+            boxes[hash_value][name] = focus_length
+
+    return sum(
+        (box_id + 1) * i * focus_length
+        for box_id, box in boxes.items()
+        for i, (_, focus_length) in enumerate(box.items(), 1)
+    )
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     initialisation_sequence = read_input("input")
     print(part_1(initialisation_sequence))
+    print(part_2(initialisation_sequence))
 
 
 if __name__ == "__main__":
