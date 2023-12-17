a3fdf57c2286be18d9db0a7355f48a56d5672835
Paul Gey <narpfel@gmx.de>
2023-12-17 11:23:28 +0100

    Solve 2023/17 part 2

--- 2023/17/src/main.rs
+++ 2023/17/src/main.rs
@@ -1,11 +1,13 @@
 use std::{
     io,
+    marker::PhantomData,
     path::Path,
 };
 
 use fnv::FnvHashMap;
 use graph::{
     CartesianPoint,
+    Point,
     ReadExt,
     World,
 };
@@ -41,7 +43,7 @@ struct DirectedPoint {
     repeat: usize,
 }
 
-impl graph::Point for DirectedPoint {
+impl Point for DirectedPoint {
     fn neighbours(self) -> impl Iterator<Item = Self>
     where
         Self: Sized,
@@ -64,28 +66,109 @@ impl graph::Point for DirectedPoint {
     }
 }
 
-#[derive(Debug, Clone)]
-struct Maze {
-    map: FnvHashMap<CartesianPoint, Tile>,
-}
-
-impl Maze {
-    fn from_file(filename: impl AsRef<Path>) -> io::Result<Self> {
-        Ok(Self { map: ReadExt::from_file(filename)? })
+impl From<DirectedPoint> for CartesianPoint {
+    fn from(value: DirectedPoint) -> Self {
+        value.point
     }
 }
 
-impl graph::World for Maze {
-    type Point = DirectedPoint;
+impl From<CartesianPoint> for DirectedPoint {
+    fn from(value: CartesianPoint) -> Self {
+        Self {
+            point: value,
+            direction: (0, 0),
+            repeat: 0,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct UltraPoint {
+    point: CartesianPoint,
+    direction: Direction,
+    repeat: usize,
+}
+
+impl Point for UltraPoint {
+    fn neighbours(self) -> impl Iterator<Item = Self>
+    where
+        Self: Sized,
+    {
+        let opposite_direction = (-self.direction.0, -self.direction.1);
+        self.point
+            .neighbours()
+            .map(move |point| {
+                let direction = point - self.point;
+                let repeat = if direction == self.direction {
+                    self.repeat + 1
+                }
+                else {
+                    1
+                };
+                UltraPoint { point, direction, repeat }
+            })
+            .filter(move |point| point.direction != opposite_direction)
+            .filter(move |point| {
+                if self.direction == (0, 0) {
+                    true
+                }
+                else if self.repeat < 4 {
+                    point.direction == self.direction
+                }
+                else if self.repeat == 10 {
+                    point.direction != self.direction
+                }
+                else {
+                    true
+                }
+            })
+    }
+}
+
+impl From<UltraPoint> for CartesianPoint {
+    fn from(value: UltraPoint) -> Self {
+        value.point
+    }
+}
+
+impl From<CartesianPoint> for UltraPoint {
+    fn from(value: CartesianPoint) -> Self {
+        Self {
+            point: value,
+            direction: (0, 0),
+            repeat: 0,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Maze<P> {
+    map: FnvHashMap<CartesianPoint, Tile>,
+    _p: PhantomData<P>,
+}
+
+impl Maze<DirectedPoint> {
+    fn from_file(filename: impl AsRef<Path>) -> io::Result<Self> {
+        Ok(Self {
+            map: ReadExt::from_file(filename)?,
+            _p: PhantomData,
+        })
+    }
+}
+
+impl<P> graph::World for Maze<P>
+where
+    P: Point + From<CartesianPoint> + Into<CartesianPoint>,
+{
+    type Point = P;
     type Tile = Tile;
 
     fn get(&self, p: &Self::Point) -> Option<Self::Tile> {
-        self.map.get(&p.point).copied()
+        self.map.get(&p.clone().into()).copied()
     }
 
     fn iter(&self) -> impl Iterator<Item = (Self::Point, &Self::Tile)> {
-        World::iter(&self.map)
-            .map(|(p, t)| (DirectedPoint { point: p, direction: (0, 0), repeat: 0 }, t))
+        World::iter(&self.map).map(|(p, t)| (P::from(p), t))
     }
 
     fn cost(&self, p: &Self::Point) -> u64 {
@@ -93,21 +176,24 @@ impl graph::World for Maze {
     }
 }
 
-fn shortest_path_length(maze: &Maze) -> u64 {
+impl From<Maze<DirectedPoint>> for Maze<UltraPoint> {
+    fn from(value: Maze<DirectedPoint>) -> Self {
+        Maze { map: value.map, _p: PhantomData }
+    }
+}
+
+fn shortest_path_length<P>(maze: &Maze<P>) -> u64
+where
+    P: Point + From<CartesianPoint>,
+    CartesianPoint: From<P>,
+{
     let start = CartesianPoint(0, 0);
     let target = maze
         .iter()
-        .map(|(p, _)| p.point)
+        .map(|(p, _)| CartesianPoint::from(p))
         .max_by_key(|&CartesianPoint(x, y)| (y, x))
         .unwrap();
-    let distance = maze.distance_with(
-        &DirectedPoint {
-            point: start,
-            direction: (0, 0),
-            repeat: 0,
-        },
-        |p| p.point == target,
-    );
+    let distance = maze.distance_with(&start.into(), |p| CartesianPoint::from(p.clone()) == target);
     distance.try_into().unwrap()
 }
 
@@ -121,10 +207,19 @@ mod tests {
         assert_eq!(shortest_path_length(&maze), 102);
         Ok(())
     }
+
+    #[test]
+    fn test_part_2() -> io::Result<()> {
+        let maze = Maze::<UltraPoint>::from(Maze::from_file("input_test")?);
+        assert_eq!(shortest_path_length(&maze), 94);
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let maze = Maze::from_file("input")?;
     println!("{}", shortest_path_length(&maze));
+    let ultra_maze = Maze::<UltraPoint>::from(maze);
+    println!("{}", shortest_path_length(&ultra_maze));
     Ok(())
 }
