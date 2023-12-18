34bd32ad473404b341d32063b03679b155a5dbaa
Paul Gey <narpfel@gmx.de>
2023-12-18 12:55:53 +0100

    2023/18: remove slow floodfill-based solution for part 1

--- 2023/18/solution.py
+++ 2023/18/solution.py
@@ -32,49 +32,8 @@ def move(point, *, distance, direction):
     return x, y
 
 
-def neighbours(point):
-    x, y = point
-    return [
-        (x - 1, y),
-        (x + 1, y),
-        (x, y - 1),
-        (x, y + 1),
-    ]
-
-
 def part_1(instructions):
-    point = 0, 0
-    path = {point}
-    for distance, direction, _ in instructions:
-        for _ in range(distance):
-            point = move(point, distance=1, direction=direction)
-            path.add(point)
-
-    min_x = min(x for x, _ in path) - 2
-    max_x = max(x for x, _ in path) + 3
-    min_y = min(y for _, y in path) - 2
-    max_y = max(y for _, y in path) + 3
-    x_range = range(min_x, max_x)
-    y_range = range(min_y, max_y)
-
-    # floodfill
-    seen = set()
-    next_points = [(min_x, min_y)]
-
-    while next_points:
-        point = next_points.pop()
-        if point not in seen:
-            seen.add(point)
-            for neighbour in neighbours(point):
-                x, y = neighbour
-                if x in x_range and y in y_range and neighbour not in path:
-                    next_points.append(neighbour)
-
-    length = max_x - min_x
-    width = max_y - min_y
-    total_area = length * width
-
-    return total_area - len(seen)
+    return part_2([(distance, direction) for distance, direction, _ in instructions])
 
 
 def parse_hex_instructions(instructions):
