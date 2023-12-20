053ee427cdccee47e5d4d08e093e9457e5a116ea
Paul Gey <narpfel@gmx.de>
2023-12-20 08:00:42 +0100

    Solve 2023/20 part 1

---
+++ 2023/20/solution.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+import math
+from collections import defaultdict
+from collections import deque
+from collections import namedtuple
+
+EXPECTED_PART_1 = 32000000
+EXPECTED_PART_1_2 = 11687500
+
+
+Module = namedtuple("Module", "name, type, targets")
+
+
+def parse_module(line):
+    sender, targets = line.strip().split(" -> ")
+    targets = targets.split(", ")
+    if sender.startswith("%"):
+        ty = "flipflop"
+    elif sender.startswith("&"):
+        ty = "conjunction"
+    else:
+        assert sender == "broadcaster"
+        ty = "broadcaster"
+    sender = sender.strip("%&")
+    return Module(name=sender, type=ty, targets=targets)
+
+
+def read_input(filename):
+    with open(filename) as lines:
+        return [parse_module(line) for line in lines]
+
+
+class FlipFlop:
+    def __init__(self, name, targets):
+        self.name = name
+        self.state = False
+        self.targets = targets
+
+    def recv(self, sender, pulse):
+        if pulse:
+            return []
+        else:
+            self.state = not self.state
+            return [(self.name, tgt, self.state) for tgt in self.targets]
+
+
+class Conjunction:
+    def __init__(self, name, inputs, targets):
+        self.name = name
+        self.inputs = {input: False for input in inputs}
+        self.targets = targets
+
+    def recv(self, sender, pulse):
+        self.inputs[sender] = pulse
+        output = not all(self.inputs.values())
+        return [(self.name, tgt, output) for tgt in self.targets]
+
+
+class Broadcaster:
+    def __init__(self, name, targets):
+        self.name = name
+        self.targets = targets
+
+    def recv(self, sender, pulse):
+        return [(self.name, tgt, pulse) for tgt in self.targets]
+
+
+def part_1(modules):
+    states = {}
+    for mod in modules:
+        if mod.type == "flipflop":
+            states[mod.name] = FlipFlop(mod.name, mod.targets)
+        elif mod.type == "conjunction":
+            inputs = [
+                m.name
+                for m in modules
+                if mod.name in m.targets
+            ]
+            states[mod.name] = Conjunction(mod.name, inputs, mod.targets)
+        else:
+            assert mod.type == "broadcaster"
+            states[mod.name] = Broadcaster(mod.name, mod.targets)
+
+    sent = defaultdict(int)
+
+    for _ in range(1000):
+        signals = deque([("button", "broadcaster", False)])
+
+        while signals:
+            sender, target, signal = signals.popleft()
+            sent[signal] += 1
+            if target in states:
+                signals.extend(states[target].recv(sender, signal))
+
+    return math.prod(sent.values())
+
+
+def test_part_1():
+    puzzle_input = read_input("input_test")
+    assert part_1(puzzle_input) == EXPECTED_PART_1
+    puzzle_input = read_input("input_test_2")
+    assert part_1(puzzle_input) == EXPECTED_PART_1_2
+
+
+def main():
+    print(part_1(read_input("input")))
+
+
+if __name__ == "__main__":
+    main()
