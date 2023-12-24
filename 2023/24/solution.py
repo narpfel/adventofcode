5f9d03b128d104b3260d73873622273cfa03b7ab
Paul Gey <narpfel@gmx.de>
2023-12-24 07:20:28 +0100

    Solve 2023/24 part 2

--- 2023/24/solution.py
+++ 2023/24/solution.py
@@ -2,7 +2,10 @@
 
 from itertools import combinations
 
+import z3
+
 EXPECTED_PART_1 = 2
+EXPECTED_PART_2 = 47
 
 
 def read_input(filename):
@@ -49,14 +52,37 @@ def part_1(hail, check_range=range(200000000000000, 400000000000000)):
     )
 
 
+def part_2(hail):
+    solver = z3.Solver()
+
+    rx, ry, rz = z3.Int("rx"), z3.Int("ry"), z3.Int("rz")
+    rvx, rvy, rvz = z3.Int("rvx"), z3.Int("rvy"), z3.Int("rvz")
+
+    for name, ((x, y, z), (vx, vy, vz)) in enumerate(hail):
+        t = z3.Int(f"t{name}")
+        solver.add(x + t * vx == rx + t * rvx)
+        solver.add(y + t * vy == ry + t * rvy)
+        solver.add(z + t * vz == rz + t * rvz)
+
+    assert solver.check() == z3.sat
+    model = solver.model()
+    return model[rx].as_long() + model[ry].as_long() + model[rz].as_long()
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input, check_range=range(7, 27)) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     hail = list(read_input("input"))
     print(part_1(hail))
+    print(part_2(hail))
 
 
 if __name__ == "__main__":
