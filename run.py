342f4b546a98618aedb4ad10a190c094d8acfb78
Paul Gey <narpfel@gmx.de>
2024-01-05 21:50:39 +0100

    run.py: implement downloading input and submitting solutions

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import datetime
 import os
 import re
 import shlex
@@ -19,8 +20,36 @@ from identify import identify
 
 FG_BOLD = "\x1B[1m"
 FG_RED = "\x1B[31m"
+FG_GREEN = "\x1B[32m"
 FG_YELLOW = "\x1B[33m"
 RESET = "\x1B[m"
+
+STDERR_DEFAULT = object()
+
+RESPONSE_TYPES = [
+    (
+        re.compile("That's the right answer!"),
+        f"{FG_BOLD}{FG_GREEN}{{match[0]}}{RESET}",
+    ),
+    (
+        re.compile(
+            r"(That's not the right answer.*?\.).*([Pp]lease wait .* before trying again\.)",
+        ),
+        f"{FG_BOLD}{FG_RED}{{match[1]}}\n{{match[2]}}{RESET}",
+    ),
+    (
+        re.compile(
+            r"You gave an answer too recently.* You have .* left to wait\.",
+        ),
+        f"{FG_BOLD}{FG_YELLOW}{{match[0]}}{RESET}",
+    ),
+    (
+        re.compile(
+            r"You don't seem to be solving the right level\.  Did you already complete it\?",
+        ),
+        f"{FG_BOLD}{FG_YELLOW}{{match[0]}}{RESET}",
+    ),
+]
 
 
 @attrs
@@ -73,6 +102,7 @@ class Runner:
         self.build_output = build_output
         self.solution_output = solution_output
         self.languages = languages
+        self.captured_output = []
 
     def run_all(self):
         stats = Statistics()
@@ -143,17 +173,26 @@ class Runner:
 
         raise FileNotFoundError(f"`{solution_dir}` does not contain a solution.")
 
-    def timed_run(self, command, cwd, output):
+    def timed_run(self, command, cwd, output, stderr=STDERR_DEFAULT):
+        if stderr is STDERR_DEFAULT:
+            stderr = output
         start_time = time.perf_counter()
-        subprocess.run(command, cwd=cwd, check=True, stdout=output, stderr=output)
-        return time.perf_counter() - start_time
+        process = subprocess.run(command, cwd=cwd, check=True, stdout=output, stderr=stderr)
+        return time.perf_counter() - start_time, process.stdout
 
     def execute(self, build_command, exection_command, cwd):
         if build_command is not None:
-            build_time = self.timed_run(build_command, cwd, self.build_output)
+            build_time, _ = self.timed_run(build_command, cwd, self.build_output)
         else:
             build_time = 0
-        execution_time = self.timed_run(exection_command, cwd, self.solution_output)
+        execution_time, output = self.timed_run(
+            exection_command,
+            cwd,
+            self.solution_output,
+            stderr=None,
+        )
+        if output is not None:
+            self.captured_output.append(output)
         return build_time, execution_time
 
     def run_executable(self, path):
@@ -232,6 +271,73 @@ def format_build_time(build_time):
         return f" (and {build_time} s of build time)."
 
 
+def get_year_day():
+    parts = Path(".").resolve().parts
+    while True:
+        try:
+            day = int(parts[-1])
+        except ValueError:
+            parts.pop()
+        else:
+            year = int(parts[-2])
+            return year, day
+
+
+def read_cookies():
+    return {
+        "Cookie": (Path(__file__).parent / ".env").read_text().strip(),
+        "User-Agent": "github.com/narpfel",
+    }
+
+
+def retrieve_input(year, day):
+    from urllib.request import Request
+    from urllib.request import urlopen
+    request = Request(
+        url=f"https://adventofcode.com/{year}/day/{day}/input",
+        headers=read_cookies(),
+    )
+    response = urlopen(request)
+    return response.read().decode()
+
+
+def wait_for_puzzle_unlock(year, day):
+    unlock_date = datetime.datetime(year, 12, day, 5, 0, tzinfo=datetime.UTC)
+    wait_end = unlock_date + datetime.timedelta(seconds=4)
+    while True:
+        now = datetime.datetime.now(tz=datetime.UTC)
+        if wait_end <= now:
+            break
+        time_to_wait = wait_end - now
+        print(f"sleeping until {unlock_date} ({time_to_wait})")
+        time.sleep(1)
+
+
+def submit_solution(year, day, part, solution):
+    from urllib.parse import urlencode
+    from urllib.request import Request
+    from urllib.request import urlopen
+    data = urlencode(dict(level=part, answer=solution))
+    request = Request(
+        url=f"https://adventofcode.com/{year}/day/{day}/answer",
+        method="POST",
+        data=data.encode(),
+        headers=read_cookies(),
+    )
+    print(f"submitting {year}/{day} part {part} with {solution!r}")
+    response = urlopen(request)
+    return response.read().decode()
+
+
+def show_submission_result(response_page):
+    for regex, format_str in RESPONSE_TYPES:
+        match = regex.search(response_page)
+        if match is not None:
+            print(format_str.format(match=match))
+            return
+    assert False, response_page
+
+
 def main(argv=None):
     parser = argparse.ArgumentParser(description="Run solutions.")
     parser.add_argument("-a", "--all", help="Run all solutions.", action="store_true")
@@ -260,9 +366,32 @@ def main(argv=None):
     )
     args = parser.parse_args(argv)
 
+    if Path("get") in args.solutions:
+        if len(args.solutions) > 1:
+            parser.error("can only get input for one problem")
+        year, day = get_year_day()
+        wait_for_puzzle_unlock(year, day)
+        try:
+            with open("input", "x", encoding="utf-8") as input_file:
+                input_file.write(retrieve_input(year, day))
+        except FileExistsError:
+            print("using cached input")
+        subprocess.check_call(["/usr/bin/less", "input"])
+        return
+
+    if Path("submit") in args.solutions:
+        if len(args.solutions) > 1:
+            parser.error("can only submit one solution at a time")
+        args.solutions = parser.get_default("solutions")
+        capture_output = subprocess.PIPE
+        submit = True
+    else:
+        capture_output = args.time
+        submit = False
+
     runner = Runner(
         build_output=args.time if args.time is not None else args.build_output,
-        solution_output=args.time,
+        solution_output=capture_output,
         languages=args.languages,
     )
     start_time = time.perf_counter()
@@ -273,19 +402,36 @@ def main(argv=None):
         for solution in args.solutions:
             stats.add(runner.run_solution(solution))
     total_duration = time.perf_counter() - start_time
-    print(
-        f"Found {stats.succeeded} solutions in {stats.execution_time} s"
-        f"{format_build_time(stats.build_time)}",
-        file=sys.stderr,
-    )
-    print(f"wall time elapsed: {total_duration} s", file=sys.stderr)
-    if stats.failed:
+    if submit and len(runner.captured_output) == 1:
+        year, day = get_year_day()
+        [output] = runner.captured_output
+        output = output.decode("utf-8")
+        print(output)
+        lines = output.splitlines()
+
+        if len(lines) == 1:
+            show_submission_result(submit_solution(year, day, part=1, solution=lines[0]))
+            if day == 25:
+                submit_solution(year, day, part=2, solution="0")
+                print(f"{FG_BOLD}{FG_GREEN}also submitting part 2...{RESET}")
+        elif len(lines) == 2:
+            show_submission_result(submit_solution(year, day, part=2, solution=lines[1]))
+        else:
+            print("weird output format, please submit manually")
+    else:
         print(
-            f"{FG_BOLD}{FG_RED}{stats.failed} solutions failed executing.{RESET}",
+            f"Found {stats.succeeded} solutions in {stats.execution_time} s"
+            f"{format_build_time(stats.build_time)}",
             file=sys.stderr,
         )
-    if stats.skipped:
-        print(f"{FG_YELLOW}{stats.skipped} solutions were skipped.{RESET}", file=sys.stderr)
+        print(f"wall time elapsed: {total_duration} s", file=sys.stderr)
+        if stats.failed:
+            print(
+                f"{FG_BOLD}{FG_RED}{stats.failed} solutions failed executing.{RESET}",
+                file=sys.stderr,
+            )
+        if stats.skipped:
+            print(f"{FG_YELLOW}{stats.skipped} solutions were skipped.{RESET}", file=sys.stderr)
 
     return stats.failed
 
