00562a673294b42247ff7014af3b384f1a9f5b16
Paul Gey <narpfel@gmx.de>
2024-01-07 22:45:40 +0100

    `md5`: update to `memmap2`

--- 2016/14/md5/src/main.rs
+++ 2016/14/md5/src/main.rs
@@ -7,7 +7,7 @@ use std::{
 fn main() -> Result<(), Box<dyn Error>> {
     let filename = args().nth(1).expect("Usage: md5 <filename>");
     let file = File::open(&filename)?;
-    let bytes = unsafe { memmap::Mmap::map(&file) }?;
+    let bytes = unsafe { memmap2::MmapOptions::new().populate().map(&file) }?;
     println!(
         "{}  {}",
         std::str::from_utf8(&md5::format_digest(md5::md5(&bytes)))?,
