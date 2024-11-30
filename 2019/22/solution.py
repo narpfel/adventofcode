63d76a3e021301f618b74a00e1040a9d9bf04745
Paul Gey <narpfel@gmx.de>
2024-11-30 19:16:09 +0100

    Solve 2019/22 part 2

--- 2019/22/solution.py
+++ 2019/22/solution.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
+import sympy
+
 PART_1_CARD_COUNT = 10_007
+
+PART_2_CARD_COUNT = 119315717514047
+PART_2_REPEAT_COUNT = 101741582076661
 
 
 def part_1(shuffle_instructions):
@@ -29,11 +34,40 @@ def part_1(shuffle_instructions):
     return cards.index(2019)
 
 
+def geometric_sum(r, n):
+    return (1 - r**n) / (1 - r)
+
+
+def part_2(shuffle_instructions, *, card_count, repeat):
+    x = sympy.symbols("x")
+    ring = sympy.GF(card_count)
+    ring_polynomial = ring[x]
+
+    target_position = ring_polynomial(x)
+    for instruction in reversed(shuffle_instructions):
+        if instruction == "deal into new stack\n":
+            target_position *= -1
+            target_position -= 1
+        elif instruction.startswith("deal with increment "):
+            increment = int(instruction.split()[-1])
+            target_position /= increment
+        elif instruction.startswith("cut "):
+            cut = int(instruction.split()[-1])
+            target_position += cut
+        else:
+            assert False
+
+    a, b = target_position.coeffs()
+
+    return int((a ** repeat * 2020 + b * geometric_sum(a, repeat)).val)
+
+
 def main():
     with open("input") as lines:
         shuffle_instructions = list(lines)
 
     print(part_1(shuffle_instructions))
+    print(part_2(shuffle_instructions, card_count=PART_2_CARD_COUNT, repeat=PART_2_REPEAT_COUNT))
 
 
 if __name__ == "__main__":
