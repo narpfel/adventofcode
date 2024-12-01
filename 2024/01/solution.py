ce65b94232ee302105347fc1633d9f724e83d217
Paul Gey <narpfel@gmx.de>
2024-12-01 06:11:03 +0100

    Solve 2024/01 part 2

--- 2024/01/solution.py
+++ 2024/01/solution.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 EXPECTED_PART_1 = 11
+EXPECTED_PART_2 = 31
 
 
 def read_input(filename):
@@ -13,13 +14,24 @@ def part_1(location_id_lists):
     return sum(abs(x - y) for x, y in zip(lhs, rhs))
 
 
+def part_2(location_id_lists):
+    lhs, rhs = location_id_lists
+    return sum(x * rhs.count(x) for x in lhs)
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     print(part_1(read_input("input")))
+    print(part_2(read_input("input")))
 
 
 if __name__ == "__main__":
