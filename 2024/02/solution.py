8986d57eee47de366427875e4a87b6859b598bfe
Paul Gey <narpfel@gmx.de>
2024-12-02 06:20:24 +0100

    Solve 2024/02 part 2

--- 2024/02/solution.py
+++ 2024/02/solution.py
@@ -3,6 +3,7 @@
 from more_itertools import windowed
 
 EXPECTED_PART_1 = 2
+EXPECTED_PART_2 = 4
 
 
 def read_input(filename):
@@ -26,13 +27,27 @@ def part_1(lines):
     )
 
 
+def part_2(lines):
+    return sum(
+        any(part_1([line[:i] + line[i + 1:]]) for i in range(len(line)))
+        for line in lines
+    )
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
-    print(part_1(read_input("input")))
+    numbers = read_input("input")
+    print(part_1(numbers))
+    print(part_2(numbers))
 
 
 if __name__ == "__main__":
