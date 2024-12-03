93800fcc9cf50b026bbf55859874bfc5f98d97e8
Paul Gey <narpfel@gmx.de>
2024-12-03 06:12:51 +0100

    Solve 2024/03 part 2

--- 2024/03/solution.py
+++ 2024/03/solution.py
@@ -3,8 +3,10 @@
 import re
 
 EXPECTED_PART_1 = 161
+EXPECTED_PART_2 = 48
 
 MULTIPLICATIONS_RE = re.compile(r"mul\((\d+),(\d+)\)")
+MULTIPLICATIONS_DO_DONT_RE = re.compile(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")
 
 
 def read_input(filename):
@@ -19,14 +21,35 @@ def part_1(code):
     )
 
 
+def part_2(code):
+    matches = MULTIPLICATIONS_DO_DONT_RE.finditer(code)
+    is_enabled = True
+    result = 0
+    for match in matches:
+        match match[0]:
+            case "do()":
+                is_enabled = True
+            case "don't()":
+                is_enabled = False
+            case _ if is_enabled:
+                result += int(match[1]) * int(match[2])
+    return result
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test_2")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     code = read_input("input")
     print(part_1(code))
+    print(part_2(code))
 
 
 if __name__ == "__main__":
