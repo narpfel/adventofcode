1d0dbbe83eda7edfabb4e8fb8273c637e03ad8ea
Paul Gey <narpfel@gmx.de>
2024-12-04 07:28:32 +0100

    Solve 2024/04 part 2

--- 2024/04/solution.py
+++ 2024/04/solution.py
@@ -3,6 +3,7 @@
 from itertools import chain
 
 EXPECTED_PART_1 = 18
+EXPECTED_PART_2 = 9
 
 
 def read_input(filename):
@@ -40,14 +41,40 @@ def part_1(lines):
     return count_xmas(lines) + sum(map(count_xmas, rotate_45_degrees(lines)))
 
 
+def count_x_mas(lines):
+    transposed_lines = list(transpose(lines))
+
+    a_positions = [
+        (y, x)
+        for y, line in enumerate(lines)
+        for x, c in enumerate(line)
+        if c == "A"
+    ]
+
+    return sum(
+        lines[y][x-1:x+2] in ("MAS", "SAM") and transposed_lines[x][y-1:y+2] in ("MAS", "SAM")
+        for y, x in a_positions
+    )
+
+
+def part_2(lines):
+    return sum(map(count_x_mas, rotate_45_degrees(lines)))
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     puzzle_input = read_input("input")
     print(part_1(puzzle_input))
+    print(part_2(puzzle_input))
 
 
 if __name__ == "__main__":
