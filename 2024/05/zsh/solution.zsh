80ff2f890314166a2cd48a1bb8aa3096926efa5e
Paul Gey <narpfel@gmx.de>
2024-12-05 18:43:06 +0100

    2024/05: add solution in zsh

---
+++ 2024/05/zsh/solution.zsh
@@ -0,0 +1,25 @@
+#!/usr/bin/env zsh
+
+for eq in "==" "!="; do
+    grep , input \
+        | tr ',' '|' \
+        | xargs -I '{}' \
+            zsh -c '
+                sorted=$( \
+                    grep '"'"'|'"'"' input \
+                        | grep -E '"'"'({})\|({})'"'"' \
+                        | tr '"'"'|'"'"' '"'"' '"'"' \
+                        | tsort \
+                        | grep -E '"'"'{}'"'"' \
+                );
+                length=$(echo "$sorted" | wc -l);
+                if [[
+                    $(echo '"'"'{}'"'"' | tr '"'"'|'"'"' '"'"','"'"'),
+                    '$eq' $(echo "$sorted" | tr '"'"'\n'"'"' '"'"','"'"')
+                ]]; then
+                    echo "$sorted" | head -n $(($length / 2 + 1)) | tail -n1
+                fi' \
+        | sed -E 's/\s+/+/g' \
+        | paste -sd+ \
+        | bc
+done
