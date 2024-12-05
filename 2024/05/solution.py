55bd1e6dd10b7f150adc4c94ccd1dab941b89666
Paul Gey <narpfel@gmx.de>
2024-12-05 06:20:28 +0100

    Solve 2024/05 part 2

--- 2024/05/solution.py
+++ 2024/05/solution.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+from functools import cmp_to_key
 from itertools import pairwise
 
 EXPECTED_PART_1 = 143
+EXPECTED_PART_2 = 123
 
 
 def read_input(filename):
@@ -23,14 +25,30 @@ def part_1(puzzle_input):
     )
 
 
+def part_2(puzzle_input):
+    orderings, updates = puzzle_input
+    key = cmp_to_key(lambda a, b: 1 if (a, b) in orderings else -1)
+    return sum(
+        sorted(update, key=key)[len(update) // 2]
+        for update in updates
+        if any((b, a) in orderings for a, b in pairwise(update))
+    )
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     puzzle_input = read_input("input")
     print(part_1(puzzle_input))
+    print(part_2(puzzle_input))
 
 
 if __name__ == "__main__":
