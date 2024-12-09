d1fcbcbdd9efd583f4a86942e6b2890c007d9e81
Paul Gey <narpfel@gmx.de>
2024-12-09 07:07:28 +0100

    Solve 2024/09 part 2

--- 2024/09/solution.py
+++ 2024/09/solution.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env pypy3
 
 EXPECTED_PART_1 = 1928
+EXPECTED_PART_2 = 2858
 
 
 def read_input(filename):
@@ -42,14 +43,44 @@ def part_1(blocks):
     return checksum(blocks)
 
 
+def part_2(blocks):
+    blocks = list(blocks)
+    j = -1
+    while j > -len(blocks):
+        while blocks[j] is EMPTY:
+            j -= 1
+        assert blocks[j] is not EMPTY
+
+        k = j
+        block = blocks[k]
+        l = 0  # noqa: E741 (`l` is not an ambiguous variable name)
+        while blocks[k] == block and k > -len(blocks):
+            k -= 1
+            l += 1
+
+        for i in range(len(blocks) + j + 1):
+            if blocks[i] is EMPTY and blocks[i:i+l] == [EMPTY] * l:
+                blocks[i:i+l], blocks[j:k:-1] = blocks[j:k:-1], blocks[i:i+l]
+                break
+        j = k
+
+    return checksum(blocks)
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     puzzle_input = read_input("input")
     print(part_1(puzzle_input))
+    print(part_2(puzzle_input))
 
 
 if __name__ == "__main__":
