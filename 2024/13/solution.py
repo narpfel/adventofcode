d42d3bb7d4567ce1b2da3fbc01d03a70bc42f4c9
Paul Gey <narpfel@gmx.de>
2024-12-13 07:18:58 +0100

    Solve 2024/13 part 2

--- 2024/13/solution.py
+++ 2024/13/solution.py
@@ -2,9 +2,13 @@
 
 import re
 
+import z3
+
 EXPECTED_PART_1 = 480
 
 NUMBER_RE = re.compile(r"\d+")
+
+A_LOT = 10000000000000
 
 
 def read_input(filename):
@@ -33,6 +37,25 @@ def part_1(machines):
     )
 
 
+def model_machine(machine):
+    n = z3.Int("n")
+    m = z3.Int("m")
+    (ax, ay), (bx, by), (px, py) = machine
+    opt = z3.Optimize()
+    opt.add(n * ax + m * bx == A_LOT + px)
+    opt.add(n * ay + m * by == A_LOT + py)
+    opt.minimize(3 * n + m)
+    if opt.check() == z3.sat:
+        model = opt.model()
+        return 3 * model[n].as_long() + model[m].as_long()
+    else:
+        return None
+
+
+def part_2(machines):
+    return sum(filter(None, map(model_machine, machines)))
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
@@ -40,6 +63,7 @@ def test_part_1():
 
 def main():
     print(part_1(read_input("input")))
+    print(part_2(read_input("input")))
 
 
 if __name__ == "__main__":
