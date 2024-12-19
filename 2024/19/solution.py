28c30ed842526e85e234f6b007297f81dacad1fd
Paul Gey <narpfel@gmx.de>
2024-12-19 07:37:50 +0100

    Solve 2024/19 part 2

--- 2024/19/solution.py
+++ 2024/19/solution.py
@@ -1,20 +1,24 @@
-#!/usr/bin/env python3
+#!/usr/bin/env pypy3
+
+from functools import cache
 
 EXPECTED_PART_1 = 6
+EXPECTED_PART_2 = 16
 
 
 def read_input(filename):
     with open(filename) as lines:
         patterns, towels = lines.read().split("\n\n")
-        return patterns.strip().split(", "), towels.strip().splitlines()
+        return tuple(patterns.strip().split(", ")), towels.strip().splitlines()
 
 
-def is_possible(patterns, towel):
+@cache
+def count_possibilities(patterns, towel):
     if not towel:
-        return True
+        return 1
     else:
-        return any(
-            is_possible(patterns, towel.removeprefix(pattern))
+        return sum(
+            count_possibilities(patterns, towel.removeprefix(pattern))
             for pattern in patterns
             if towel.startswith(pattern)
         )
@@ -22,7 +26,12 @@ def is_possible(patterns, towel):
 
 def part_1(puzzle_input):
     patterns, towels = puzzle_input
-    return sum(is_possible(patterns, towel) for towel in towels)
+    return sum(count_possibilities(patterns, towel) != 0 for towel in towels)
+
+
+def part_2(puzzle_input):
+    patterns, towels = puzzle_input
+    return sum(count_possibilities(patterns, towel) for towel in towels)
 
 
 def test_part_1():
@@ -30,9 +39,15 @@ def test_part_1():
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     puzzle_input = read_input("input")
     print(part_1(puzzle_input))
+    print(part_2(puzzle_input))
 
 
 if __name__ == "__main__":
