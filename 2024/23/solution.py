20af58993f927e039f97f5110742d5dfa5263199
Paul Gey <narpfel@gmx.de>
2024-12-23 16:54:10 +0100

    Solve 2024/23 part 2

--- 2024/23/solution.py
+++ 2024/23/solution.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from itertools import combinations
 
 EXPECTED_PART_1 = 7
+EXPECTED_PART_2 = "co,de,ka,ta"
 
 
 def read_input(filename):
@@ -36,14 +37,44 @@ def part_1(connections):
     )
 
 
+def part_2(connections):
+    # I have no idea why this works, because it absolutely doesn’t find *all*
+    # cliques (e. g. part 1 can’t be solved with this algorithm).
+    cliques = set()
+    done = set()
+    for c in connections:
+        if c in done:
+            continue
+        todo = set(connections[c])
+        seen = {c}
+        while todo:
+            c = todo.pop()
+            if all(c in connections[s] for s in seen):
+                seen.add(c)
+                todo.update(
+                    c
+                    for c in connections[c]
+                    if c not in seen
+                )
+        cliques.add(frozenset(seen))
+        done.update(seen)
+    return ",".join(sorted(max(cliques, key=len)))
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     connections = read_input("input")
     print(part_1(connections))
+    print(part_2(connections))
 
 
 if __name__ == "__main__":
