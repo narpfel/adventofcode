b64c8dd586682e75575ad2f1661bc7a78191a455
Paul Gey <narpfel@gmx.de>
2025-12-01 06:42:41 +0100

    Solve 2025/01 part 2

--- 2025/01/solution.py
+++ 2025/01/solution.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 EXPECTED_PART_1 = 3
+EXPECTED_PART_2 = 6
 
 
 def read_input(filename):
@@ -18,14 +19,33 @@ def part_1(rotations):
     return stopped_at_zero
 
 
+def part_2(rotations):
+    position = 50
+    was_zero = 0
+    for rotation in rotations:
+        direction = -1 if rotation < 0 else 1
+        for _ in range(abs(rotation)):
+            position += direction
+            position %= 100
+            if position == 0:
+                was_zero += 1
+    return was_zero
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     rotations = read_input("input")
     print(part_1(rotations))
+    print(part_2(rotations))
 
 
 if __name__ == "__main__":
