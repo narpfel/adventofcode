407cd82dcbd2d8e8d2e998e6c64b41d8c9947c43
Paul Gey <narpfel@gmx.de>
2025-12-04 06:21:04 +0100

    Solve 2025/04 part 2

--- 2025/04/solution.py
+++ 2025/04/solution.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 EXPECTED_PART_1 = 13
+EXPECTED_PART_2 = 43
 
 
 def read_input(filename):
@@ -34,14 +35,32 @@ def part_1(tiles):
     return sum(1 for _ in accessible(tiles))
 
 
+def part_2(tiles):
+    removed_tiles_count = 0
+    while True:
+        removable = list(accessible(tiles))
+        if not removable:
+            break
+        removed_tiles_count += len(removable)
+        for tile in removable:
+            tiles.remove(tile)
+    return removed_tiles_count
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     tiles = read_input("input")
     print(part_1(tiles))
+    print(part_2(tiles))
 
 
 if __name__ == "__main__":
