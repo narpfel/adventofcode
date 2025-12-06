53f6e278f133e70ba55eb9d36e4232e4c6931767
Paul Gey <narpfel@gmx.de>
2025-12-06 06:52:29 +0100

    Solve 2025/06 part 2

--- 2025/06/solution.py
+++ 2025/06/solution.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 import math
+from itertools import takewhile
+from itertools import zip_longest
 
 EXPECTED_PART_1 = 4277556
+EXPECTED_PART_2 = 3263827
 
 OPERATORS = {"+": sum, "*": math.prod}
 
@@ -12,8 +15,8 @@ def read_input(filename):
         return input_file.read().splitlines()
 
 
-def transpose(xss):
-    return zip(*xss)
+def transpose(xss, fillvalue=" "):
+    return zip_longest(*xss, fillvalue=fillvalue)
 
 
 def part_1(problems):
@@ -23,14 +26,29 @@ def part_1(problems):
     )
 
 
+def part_2(problems):
+    numbers = map("".join, transpose(problems[:-1]))
+    operators = problems[-1]
+    return sum(
+        OPERATORS[op](map(int, takewhile(lambda col: not col.isspace(), numbers)))
+        for op in operators.split()
+    )
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     problems = read_input("input")
     print(part_1(problems))
+    print(part_2(problems))
 
 
 if __name__ == "__main__":
