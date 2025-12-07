d136376472ccf562c917e8e625f1b11e35089a47
Paul Gey <narpfel@gmx.de>
2025-12-07 14:25:17 +0100

    Solve 2025/07 part 2

--- 2025/07/solution.py
+++ 2025/07/solution.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+from collections import Counter
+
 EXPECTED_PART_1 = 21
+EXPECTED_PART_2 = 40
 
 
 def read_input(filename):
@@ -8,30 +11,37 @@ def read_input(filename):
         return [line.strip() for line in lines]
 
 
-def part_1(manifold):
-    beam_positions = {manifold[0].index("S")}
+def solve(manifold):
+    beam_positions = Counter([manifold[0].index("S")])
     splits = 0
     for line in manifold[1:]:
-        new_beam_positions = set()
+        new_beam_positions = Counter()
         for x in beam_positions:
             if line[x] == "^":
-                new_beam_positions.add(x - 1)
-                new_beam_positions.add(x + 1)
+                new_beam_positions[x - 1] += beam_positions[x]
+                new_beam_positions[x + 1] += beam_positions[x]
                 splits += 1
             else:
-                new_beam_positions.add(x)
+                new_beam_positions[x] += beam_positions[x]
         beam_positions = new_beam_positions
-    return splits
+    return splits, beam_positions.total()
 
 
 def test_part_1():
     puzzle_input = read_input("input_test")
-    assert part_1(puzzle_input) == EXPECTED_PART_1
+    assert solve(puzzle_input)[0] == EXPECTED_PART_1
+
+
+def test_part_2():
+    puzzle_input = read_input("input_test")
+    assert solve(puzzle_input)[1] == EXPECTED_PART_2
 
 
 def main():
     manifold = read_input("input")
-    print(part_1(manifold))
+    part_1, part_2 = solve(manifold)
+    print(part_1)
+    print(part_2)
 
 
 if __name__ == "__main__":
