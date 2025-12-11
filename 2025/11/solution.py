1c7afb6dfb53ec7721d24407370aec3157736211
Paul Gey <narpfel@gmx.de>
2025-12-11 17:49:54 +0100

    Solve 2025/11 part 2

--- 2025/11/solution.py
+++ 2025/11/solution.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+from functools import cache
+
 EXPECTED_PART_1 = 5
+EXPECTED_PART_2 = 2
 
 
 def read_input(filename):
@@ -24,14 +27,36 @@ def part_1(connections):
     return path_count
 
 
+def part_2(connections):
+    @cache
+    def count_paths(start, end):
+        if start == end:
+            return 1
+        return sum(
+            count_paths(p, end)
+            for p in connections.get(start, [])
+        )
+
+    return (
+        count_paths("svr", "dac") * count_paths("dac", "fft") * count_paths("fft", "out")
+        + count_paths("svr", "fft") * count_paths("fft", "dac") * count_paths("dac", "out")
+    )
+
+
 def test_part_1():
     puzzle_input = read_input("input_test")
     assert part_1(puzzle_input) == EXPECTED_PART_1
 
 
+def test_part_2():
+    puzzle_input = read_input("input_test_2")
+    assert part_2(puzzle_input) == EXPECTED_PART_2
+
+
 def main():
     connections = read_input("input")
     print(part_1(connections))
+    print(part_2(connections))
 
 
 if __name__ == "__main__":
